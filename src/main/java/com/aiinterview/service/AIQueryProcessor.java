e5e75e083b018a40a997cc8b13bda7f342e21358
Avanish <avanishghosh@gmail.com>
2025-04-04 18:17:57 +0530

    added test controller

--- src/main/java/com/aiinterview/service/AIQueryProcessor.java
+++ src/main/java/com/aiinterview/service/AIQueryProcessor.java
@@ -2,16 +2,15 @@ package com.aiinterview.service;
 
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class AIQueryProcessor {
     private final OpenAiService openAiService;
 
     public AIQueryProcessor() {
-        this.openAiService = new OpenAiService(System.getenv("OPENAI_API_KEY"));
+        this.openAiService = new OpenAiService();
     }
 
     public String processQuery(String query) {
-        return openAiService.createCompletion(query);
+        return openAiService.getCompletion(query);
     }
 } 
